15b69276ae0949c6adac1f34ba0d97339e88a7f0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-06-18 10:06:58 +0000

    Deploy thimodprbasha/Portfolio to github.com/thimodprbasha/Portfolio.git:gh-pages

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Portfolio/sw.js', { scope: '/Portfolio/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gitprofile/sw.js', { scope: '/gitprofile/' })})}
